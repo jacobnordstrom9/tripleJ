3870f0b2c7cc07d771aa47d19d8175ad6b13d0d9
Jason Humphrey <sjasonhumphrey@yahoo.com>
2021-05-13 13:31:05 -0500

    fix gitignore

--- scripts/api.js
+++ scripts/api.js
@@ -1,2 +1,2 @@
-var apiKey1 = 'fd97d2144f719acfbc1b674ebc8649c3'
-var apiKey2 = 'zEFeWI13fDAJn3ewgAQ6L5Wqv182kWCNgy8Uwgif'
+var apiKey1 = 'fd97d2144f719acfbc1b674ebc8649c3';
+var apiKey2 = 'zEFeWI13fDAJn3ewgAQ6L5Wqv182kWCNgy8Uwgif';
